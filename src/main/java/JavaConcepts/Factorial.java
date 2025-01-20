ff9e3a7b298b2c1378b7106e98128b3618c03c8e
Ritika Joshi <me.ritikajoshi@gmail.com>
2025-01-20 17:15:14 +0545

    changes

--- src/main/java/JavaConcepts/Factorial.java
+++ src/main/java/JavaConcepts/Factorial.java
@@ -5,9 +5,8 @@ public class Factorial {
     public static void main(String[] args) {
         int fact=1;
         int num=5;
-        for(int i=1 ; i<=num; i++){
+        for(int i=1 ; i<=num; i++)
             fact=fact*i;
-        }
         System.out.println(fact);
     }
 }
