ff8bc5450d5040d3540b06fe5be84dcf0d2da5c2
me-ritikajoshi <me.ritikajoshi@gmail.com>
2025-02-18 22:23:32 +0545

    Example for Number System conversion

---
+++ src/main/java/JavaContent/NumberSystems.java
@@ -0,0 +1,22 @@
+package JavaContent;
+
+public class NumberSystems {
+    public static void main(String[] args) {
+        /*
+        decimals are given as usual numbers
+        binary is given by 0b
+        octal is given by 0
+        and hexadecimal are given by 0x
+        */
+        int dec = 152;
+        int bin = 0b10011;
+        int oct = 0230;
+        int hex = 0x1234;
+
+        System.out.println("In decimal: " +dec);
+        System.out.println("In binary: " +bin);
+        System.out.println("In octal: " +oct);
+        System.out.println("In hexadecimal: " +hex);
+
+    }
+}
