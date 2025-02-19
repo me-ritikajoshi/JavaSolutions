9a95adeb47b139c459487397d3d971fdd36edc91
me-ritikajoshi <me.ritikajoshi@gmail.com>
2025-02-19 22:19:31 +0545

    Example for CircumferenceOfCircle

--- src/main/java/JavaContent/CircumferenceOfCircle.java
+++ src/main/java/JavaContent/CircumferenceOfCircle.java
@@ -7,7 +7,7 @@ public class CircumferenceOfCircle {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the radius: ");
         int r = sc.nextInt();
-        double circumference = Math.PI*r*r;
+        double circumference = 2*Math.PI*r;
 
         System.out.println("Circumference of circle: " +circumference);
     }
