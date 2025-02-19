91feb38f6ce0d75f6190daf6af7abd5cfb0efa43
me-ritikajoshi <me.ritikajoshi@gmail.com>
2025-02-19 21:04:36 +0545

    Example for TernaryOperators

---
+++ src/main/java/JavaContent/TernaryOperators.java
@@ -0,0 +1,14 @@
+package JavaContent;
+
+public class TernaryOperators {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int max;
+
+        max= (num1 > num2) ? num1 : num2 ;
+
+        System.out.println(max);
+
+    }
+}
