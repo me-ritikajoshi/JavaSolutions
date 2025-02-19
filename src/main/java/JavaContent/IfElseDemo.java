9edaf33af73414dee6b6c2a1b86cba3d08cc7534
me-ritikajoshi <me.ritikajoshi@gmail.com>
2025-02-19 22:53:57 +0545

    Example for ifElse demo

---
+++ src/main/java/JavaContent/IfElseDemo.java
@@ -0,0 +1,14 @@
+package JavaContent;
+
+public class IfElseDemo {
+    public static void main(String[] args) {
+        int amtOfMoneyInAccount = 100;
+        int itemPrice = 200;
+
+        if(amtOfMoneyInAccount < itemPrice){
+            System.out.println("Not enough balance");
+        }else{
+            System.out.println("Item purchased");
+        }
+    }
+}
