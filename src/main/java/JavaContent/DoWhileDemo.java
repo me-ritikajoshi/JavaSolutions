ec57482eebd1f4405113ffb2ca1cf150564705b7
me-ritikajoshi <me.ritikajoshi@gmail.com>
2025-02-20 22:31:53 +0545

    Example for do-while loop demo

---
+++ src/main/java/JavaContent/DoWhileDemo.java
@@ -0,0 +1,10 @@
+package JavaContent;
+
+public class DoWhileDemo {
+    public static void main(String[] args) {
+        int counter = 0;
+        do {
+            System.out.println("Counter: " + counter++);
+        }while (counter<5);
+    }
+}
