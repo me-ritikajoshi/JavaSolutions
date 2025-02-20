617cd4fbc47c23b69239e009dddcb34b54468ae7
me-ritikajoshi <me.ritikajoshi@gmail.com>
2025-02-20 22:42:30 +0545

    Example for nested each loop demo

---
+++ src/main/java/JavaContent/NestedLoop.java
@@ -0,0 +1,12 @@
+package JavaContent;
+
+public class NestedLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("*");
+            for (int j = 0; j <3 ; j++) {
+                System.out.println("**");
+            }
+        }
+    }
+}
