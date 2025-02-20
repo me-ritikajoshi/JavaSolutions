f74849c495375d675594c2243789bdbb3628379b
me-ritikajoshi <me.ritikajoshi@gmail.com>
2025-02-20 21:57:53 +0545

    Example for ifelse

---
+++ src/main/java/JavaContent/AdminAndGuestMode.java
@@ -0,0 +1,21 @@
+package JavaContent;
+
+import java.util.Scanner;
+
+public class AdminAndGuestMode {
+    public static void main(String[] args) {
+       String admin = "--admin";
+       String guest = "--guest";
+
+       Scanner sc = new Scanner(System.in);
+       String inp = sc.next();
+
+       if(inp.contains(admin) && inp.contains((guest))){
+           System.out.println("Please select either admin or guest mode");
+       } else if (inp.contains(admin)) {
+           System.out.println("hello admin");
+       }else {
+           System.out.println("hello guest");
+       }
+    }
+}
