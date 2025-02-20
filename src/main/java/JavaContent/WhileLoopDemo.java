f45eae8153e28bd503c6ac0d2a6b749c8434c165
me-ritikajoshi <me.ritikajoshi@gmail.com>
2025-02-20 22:31:35 +0545

    Example for while loop demo

---
+++ src/main/java/JavaContent/WhileLoopDemo.java
@@ -0,0 +1,11 @@
+package JavaContent;
+
+public class WhileLoopDemo {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter < 3){
+            System.out.println(counter++);
+
+        }
+    }
+}
