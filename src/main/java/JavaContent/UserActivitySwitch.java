f9ec79b94caeb4890fe7c80e44a71929b6980896
me-ritikajoshi <me.ritikajoshi@gmail.com>
2025-02-20 22:25:02 +0545

    Example for User Activity SwitchStatement

---
+++ src/main/java/JavaContent/UserActivitySwitch.java
@@ -0,0 +1,32 @@
+package JavaContent;
+
+import java.util.Scanner;
+
+public class UserActivitySwitch {
+    public static void main(String[] args) {
+        System.out.println("enter an option: ");
+        Scanner sc = new Scanner(System.in);
+        String inp = sc.nextLine();
+
+        switch (inp){
+            case "login":
+                System.out.println("Please enter your name");
+                break;
+            case "sign_up":
+                System.out.println("Welcome");
+                break;
+            case "terminate_program":
+                System.out.println("Thank you! Bye");
+                break;
+            case "main_menu":
+                System.out.println("Main menu");
+                break;
+            case "about_app":
+                System.out.println("appppppp");
+                break;
+            default:
+                System.out.println("Please enetr one value ");
+        }
+
+    }
+}
