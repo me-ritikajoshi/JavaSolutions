0ca5d2b2563528f2e6de5f3f944c5d8559c89828
me-ritikajoshi <me.ritikajoshi@gmail.com>
2025-02-20 22:27:06 +0545

    Example for random number

---
+++ src/main/java/JavaContent/RandomDemo.java
@@ -0,0 +1,12 @@
+package JavaContent;
+
+import java.util.Random;
+
+public class RandomDemo {
+    public static void main(String[] args) {
+        Random r = new Random();
+        System.out.println(r.nextInt());
+        System.out.println(r.nextInt(100));
+        System.out.println(r.nextInt(100)+1);
+    }
+}
