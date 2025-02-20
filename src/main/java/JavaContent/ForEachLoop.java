ef94b3d5dbcb31bb844f101db29b0cf117139ed8
me-ritikajoshi <me.ritikajoshi@gmail.com>
2025-02-20 22:42:14 +0545

    Example for foreach loop demo

---
+++ src/main/java/JavaContent/ForEachLoop.java
@@ -0,0 +1,12 @@
+package JavaContent;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+        int[] arr ={1,2,3};
+        int sum = 0;
+        for (int num : arr){
+            sum += num;
+        }
+        System.out.println("Sum: " +sum);
+    }
+}
