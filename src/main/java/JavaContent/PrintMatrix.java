1ef3efa76a542886f7ba93dbec069b155d8a6174
me-ritikajoshi <me.ritikajoshi@gmail.com>
2025-02-21 21:30:49 +0545

    Example for PrintMatrix

--- src/main/java/JavaContent/PrintMatrix.java
+++ src/main/java/JavaContent/PrintMatrix.java
@@ -10,7 +10,7 @@ public class PrintMatrix {
 
         for (int i = 0; i < matrix.length; i++) {
                 for (int j = 0; j < matrix[i].length; j++) {
-                    System.out.print(matrix[i][j]);
+                    System.out.print(matrix[i][j] + "\t");
                 }
             System.out.println();
         }
