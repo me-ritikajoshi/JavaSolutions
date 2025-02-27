fe8067917c92b1de55f751f678994a6ad74ec0c9
me-ritikajoshi <me.ritikajoshi@gmail.com>
2025-02-27 22:35:33 +0545

    if else demo example

--- src/main/java/JavaContent/BasicConcepts/IfElseDemo.java
+++ src/main/java/JavaContent/BasicConcepts/IfElseDemo.java
@@ -2,13 +2,31 @@ package JavaContent.BasicConcepts;
 
 public class IfElseDemo {
     public static void main(String[] args) {
-        int amtOfMoneyInAccount = 100;
-        int itemPrice = 200;
+//        int amtOfMoneyInAccount = 100;
+//        int itemPrice = 200;
+//
+//        if(amtOfMoneyInAccount < itemPrice){
+//            System.out.println("Not enough balance");
+//        }else{
+//            System.out.println("Item purchased");
+//        }
 
-        if(amtOfMoneyInAccount < itemPrice){
-            System.out.println("Not enough balance");
-        }else{
-            System.out.println("Item purchased");
+        boolean isAlien = false;
+        if(isAlien == false){
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }else {
+            System.out.println("It is an alien !");
+        }
+
+        int topScore = 100;
+        if (topScore != 100){
+            System.out.println("You've got the highest score");
+        }
+
+        int secondTopScore = 60;
+        if(topScore > secondTopScore && topScore <= 100){
+            System.out.println("Greater than second top score and less than 100");
         }
     }
 }
