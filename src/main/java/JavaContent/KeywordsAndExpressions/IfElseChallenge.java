c31995629dec6ec2e33d252c16ec26fca74f4071
me-ritikajoshi <me.ritikajoshi@gmail.com>
2025-02-28 19:14:57 +0545

    IfElseChallenge using method

--- src/main/java/JavaContent/KeywordsAndExpressions/IfElseChallenge.java
+++ src/main/java/JavaContent/KeywordsAndExpressions/IfElseChallenge.java
@@ -2,42 +2,36 @@ package JavaContent.KeywordsAndExpressions;
 
 public class IfElseChallenge {
     public static void main(String[] args) {
-        boolean gameOver=  true;
+
+        boolean gameOver = true;
         int score = 800;
         int levelCompleted = 5;
-        int bonus = 500;
+        int bonus = 100;
 
-        int finalScore = score;
-
-        if (gameOver){
-            finalScore += (levelCompleted * bonus);
-            finalScore += 1000;
-            System.out.println("Your final score is: " +finalScore);
-        }
-
-//        boolean newGameOver=  true;
-//        int newScore = 10000;
-//        int newLevelCompleted = 8;
-//        int newBonus = 200;
-//
-//        int newFinalScore = newScore;
-//
-//        if (newGameOver){
-//            newFinalScore += (newLevelCompleted * newBonus);
-//            System.out.println("Your new final score is: " +newFinalScore);
-//        }
+        int hightScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println(hightScore);
 
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
 
-        finalScore = score;
+        System.out.println("The next hightScore is: " +
+        calculateScore(gameOver, score, levelCompleted, bonus));
+
+
+    }
+
+    public static int calculateScore( boolean gameOver, int score, int levelCompleted, int bonus){
+//        boolean gameOver=  true;
+//        int score = 800;
+//        int levelCompleted = 5;
+//        int bonus = 100;
+        int finalScore = score;
 
         if (gameOver){
             finalScore += (levelCompleted * bonus);
-            System.out.println("Your final score is: " +finalScore);
+            finalScore += 1000;
         }
-
-
+        return finalScore;
     }
 }
