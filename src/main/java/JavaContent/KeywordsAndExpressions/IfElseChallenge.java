5ca540ef2335edd3e3afa811d287afd91327ff29
me-ritikajoshi <me.ritikajoshi@gmail.com>
2025-02-28 18:27:11 +0545

    IfElseChallenge

--- src/main/java/JavaContent/KeywordsAndExpressions/IfElseChallenge.java
+++ src/main/java/JavaContent/KeywordsAndExpressions/IfElseChallenge.java
@@ -2,6 +2,42 @@ package JavaContent.KeywordsAndExpressions;
 
 public class IfElseChallenge {
     public static void main(String[] args) {
+        boolean gameOver=  true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 500;
+
+        int finalScore = score;
+
+        if (gameOver){
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score is: " +finalScore);
+        }
+
+//        boolean newGameOver=  true;
+//        int newScore = 10000;
+//        int newLevelCompleted = 8;
+//        int newBonus = 200;
+//
+//        int newFinalScore = newScore;
+//
+//        if (newGameOver){
+//            newFinalScore += (newLevelCompleted * newBonus);
+//            System.out.println("Your new final score is: " +newFinalScore);
+//        }
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        finalScore = score;
+
+        if (gameOver){
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your final score is: " +finalScore);
+        }
+
 
     }
 }
