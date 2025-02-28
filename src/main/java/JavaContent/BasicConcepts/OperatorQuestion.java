dc78dda693ca307e4b460b5437c32ca49e102612
me-ritikajoshi <me.ritikajoshi@gmail.com>
2025-02-28 14:43:45 +0545

    completed operator question

--- src/main/java/JavaContent/BasicConcepts/OperatorQuestion.java
+++ src/main/java/JavaContent/BasicConcepts/OperatorQuestion.java
@@ -2,14 +2,15 @@ package JavaContent.BasicConcepts;
 
 public class OperatorQuestion {
     public static void main(String[] args) {
-        double val1 = 20.00;
-        double val2= 80.00;
-        double result = ((val1 + val2) * 100.00) % 2;
+        double val1 = 20.00d;
+        double val2 = 80.00d;
+        double result = ((val1 + val2) * 100.00) % 40.00d;
         boolean isResult = (result == 0.00) ? true : false;
 
         System.out.println(isResult);
-
-
+        if (!isResult) {
+            System.out.println("Got some remainer");
+        }
 
     }
 }
