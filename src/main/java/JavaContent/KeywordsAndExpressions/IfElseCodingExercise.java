4055fd398604138157940a9b64009256eeb43bcc
me-ritikajoshi <me.ritikajoshi@gmail.com>
2025-03-03 21:30:42 +0545

    if else and method overloading

--- src/main/java/JavaContent/KeywordsAndExpressions/IfElseCodingExercise.java
+++ src/main/java/JavaContent/KeywordsAndExpressions/IfElseCodingExercise.java
@@ -5,60 +5,65 @@ public class IfElseCodingExercise {
         System.out.println(checkNumber(0));
         printConversion(80);
         printMegaBytesAndKiloBytes(2000);
-        System.out.println("Should wake up? "+ shouldWakeUp(true, 10));
-        System.out.println("Is leap year? "+ isLeapYear(2000));
-        System.out.println("Is 3 decimal place equal?" + areEqualByThreeDecimalPlace(5.123,5.1234));
-        System.out.println("Has equal sum? " + hasEqualSum(1,-1,0));
-        System.out.println("Has teen? " + hasTeen(5,15,99));
+        System.out.println("Should wake up? " + shouldWakeUp(true, 10));
+        System.out.println("Is leap year? " + isLeapYear(2000));
+        System.out.println("Is 3 decimal place equal?" + areEqualByThreeDecimalPlace(5.123, 5.1234));
+        System.out.println("Has equal sum? " + hasEqualSum(1, -1, 0));
+        System.out.println("Has teen? " + hasTeen(5, 15, 99));
+        System.out.println("Area: " + area(5));
+        System.out.println("Area: " + area(5, 8));
+        printYearsAndDays(1000000);
     }
-    public static String checkNumber(int number){
-        if (number > 0){
+
+    public static String checkNumber(int number) {
+        if (number > 0) {
             return "positive";
         } else if (number < 0) {
             return "negative";
-        }else {
+        } else {
             return "zero";
         }
     }
 
-    public static long toMilesPerHour(double kilometersPerHour){
-        if (kilometersPerHour < 0){
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
             return -1;
         }
         return Math.round(kilometersPerHour / 1.609);
     }
-    public static void printConversion(double kilometersPerHour){
-        if(kilometersPerHour < 0){
+
+    public static void printConversion(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
             System.out.println("Invalid value");
         }
         long milesPerHour = toMilesPerHour(kilometersPerHour);
-        System.out.println(kilometersPerHour + " km/h= " +milesPerHour +" mi/h");
+        System.out.println(kilometersPerHour + " km/h= " + milesPerHour + " mi/h");
     }
 
 
-    public static void printMegaBytesAndKiloBytes(int kiloBytes){
-        if (kiloBytes < 0){
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes < 0) {
             System.out.println("Invalid Value");
         }
         int mb = kiloBytes / 1024;
         int kb = kiloBytes % 1024;
-        System.out.println(kiloBytes + "KB= " + mb +"MB and " + kb + "KB" );
+        System.out.println(kiloBytes + "KB= " + mb + "MB and " + kb + "KB");
     }
 
-    public static boolean shouldWakeUp(boolean isBarking, int hourOfDay){
-        if (hourOfDay < 0 || hourOfDay > 23){
+    public static boolean shouldWakeUp(boolean isBarking, int hourOfDay) {
+        if (hourOfDay < 0 || hourOfDay > 23) {
             return false;
         }
-            return isBarking && (hourOfDay < 8 || hourOfDay >22);
+        return isBarking && (hourOfDay < 8 || hourOfDay > 22);
     }
 
-    public static boolean isLeapYear(int year){
-        if (year > 0 && year <= 9999){
+    public static boolean isLeapYear(int year) {
+        if (year > 0 && year <= 9999) {
             return false;
         }
-        if (year % 4 == 0){
-            if (year % 100 == 0){
-                if (year % 400 == 0){
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
                     return true;
                 }
             }
@@ -66,35 +71,60 @@ public class IfElseCodingExercise {
         return false;
     }
 
-    public static boolean areEqualByThreeDecimalPlace(double num1, double num2){
-        long n1= (long) num1 *1000;
-        long n2= (long) num2 *1000;
+    public static boolean areEqualByThreeDecimalPlace(double num1, double num2) {
+        long n1 = (long) num1 * 1000;
+        long n2 = (long) num2 * 1000;
         return n1 == n2;
     }
 
-    public static boolean hasEqualSum(int n1, int n2, int n3){
+    public static boolean hasEqualSum(int n1, int n2, int n3) {
         int sumOfTwo = n1 + n2;
         return sumOfTwo == n3;
     }
 
-    public static boolean isTeen(int num){
-        return num >= 13 && num <=19;
+    public static boolean isTeen(int num) {
+        return num >= 13 && num <= 19;
     }
 
-    public static boolean hasTeen(int a, int b, int c){
+    public static boolean hasTeen(int a, int b, int c) {
         return isTeen(a) || isTeen(b) || isTeen(c);
     }
 
-    public static double area(double radius){
-        if (radius < 0){
+    public static double area(double radius) {
+        if (radius < 0) {
             return -1;
         }
         return Math.PI * radius * radius;
     }
-    public static double area(double x, double y){
-        if (x < 0 || y < 0){
+
+    public static double area(double x, double y) {
+        if (x < 0 || y < 0) {
             return -1;
         }
         return x * y;
     }
+
+    public static void printYearsAndDays(long minutes) {
+        if (minutes < 0) {
+            System.out.println("Invalid input");
+            return;
+        }
+        long year = minutes / (60 * 24 * 365);
+        long days = (minutes / (60 * 24)) % 365;
+        System.out.println(minutes + " minutes= " + year + " year " + days + " days ");
+    }
+
+    public static void printEqual(int a, int b, int c) {
+        if (a < 0 || b < 0 || c < 0) {
+            System.out.println("Invalid Value");
+        } else if (a == b && b == c) {
+            System.out.println("All values are equal");
+//        } else if () {
+//
+//        }
+        }
+
+    }
 }
+
+
--- src/main/java/JavaContent/KeywordsAndExpressions/MethodOverloadingChallenge.java
+++ src/main/java/JavaContent/KeywordsAndExpressions/MethodOverloadingChallenge.java
@@ -2,13 +2,46 @@ package JavaContent.KeywordsAndExpressions;
 
 public class MethodOverloadingChallenge {
     public static void main(String[] args) {
+        System.out.println("Inches into cm : " + convertToCentimeters(50));
+        System.out.println("Feet and inches into cm : " +convertToCentimeters(5,6));
+
+        System.out.println( getDurationString(-3945));
+        System.out.println(getDurationString(-65, 45));
+        System.out.println(getDurationString( 65, 145));
 
     }
-    public static double convertToCentimeters(int height){
-        return height *  1.1d;
+    public static double convertToCentimeters(int inches){
+        return inches *  2.5d;
     }
 
     public static double convertToCentimeters(int feet , int inches){
-        return feet *  1.1d;
+//        int feetIntoInches = feet * 12;
+//        int totalInches = feetIntoInches + inches;
+//        double result = totalInches * 2.5d;
+//        convertToCentimeters(totalInches);
+//        return result;
+
+        return ((12 * feet ) + inches )*  2.5d;
+    }
+
+
+    public static String getDurationString(int seconds){
+        if (seconds < 0){
+            return "Invalid input for seconds " + seconds ;
+        }
+        int minutes = seconds / 60;
+        return getDurationString(seconds / 60 , seconds % 60);
+    }
+
+    public static String getDurationString(int minutes, int seconds){
+        if (minutes < 0){
+            return "Invalid input for minutes " + minutes ;
+        }
+
+        int hours = minutes / 60;
+        int remainingMinutes = minutes % 60;
+        int remaningSeconds = seconds % 60;
+
+        return hours + " hrs " +remainingMinutes + " mins " + remaningSeconds +" seconds ";
     }
 }
