a9f8ea4d7253b23d50e693f7e8abbaa83ad8359e
me-ritikajoshi <me.ritikajoshi@gmail.com>
2025-03-06 21:45:39 +0545

    while loop challenge

--- src/main/java/JavaContent/ControlFlow/WhileLoopChallenge.java
+++ src/main/java/JavaContent/ControlFlow/WhileLoopChallenge.java
@@ -2,13 +2,22 @@ package JavaContent.ControlFlow;
 
 public class WhileLoopChallenge {
     public static void main(String[] args) {
-        int num = 5;
+        int num = 4;
+        int countEven = 0, countOdd = 0;
         while (num <= 20) {
-            if (num % 2 == 0) {
-                System.out.println(num + " is an even number");
-            }
             num++;
+            if (! isEvenNumber(num)) {
+                countOdd += 1;
+                continue;
+            }
+            countEven += 1;
+            if (countEven >= 5){
+                break;
+            }
+            System.out.println(num + " is an even number");
         }
+        System.out.println("Even number count: " + countEven);
+        System.out.println("Pdd number count: " + countOdd);
 
     }
     public static boolean isEvenNumber( int num){
