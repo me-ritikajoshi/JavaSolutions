e47c4b6d3c3c7b78eb9ee6083964aa7c73fe7673
me-ritikajoshi <me.ritikajoshi@gmail.com>
2025-03-13 20:59:45 +0545

    control flow problems

--- src/main/java/JavaContent/ControlFlow/ControlFlowChallenge.java
+++ src/main/java/JavaContent/ControlFlow/ControlFlowChallenge.java
@@ -1,18 +1,26 @@
 package JavaContent.ControlFlow;
 
+import java.util.Scanner;
+
 public class ControlFlowChallenge {
     public static void main(String[] args) {
         printNumberInWord(90);
-        System.out.println(isLeapYear(2000));
-        System.out.println(getDaysInMonth(2, 2000));
-        System.out.println(sumOdd(1, 100));
-        System.out.println(isPalindrome(12321));
-        System.out.println(sumFirstAndLastDigit(111));
-        System.out.println(getEvenDigitSum(123456789));
+        System.out.println("Is leap year?" +isLeapYear(2000));
+        System.out.println("Days of mth"+ getDaysInMonth(2, 2000));
+        System.out.println("Sum of odd"+ sumOdd(1, 100));
+        System.out.println("Is palindrome? "+ isPalindrome(12321));
+        System.out.println("Sum of first and last digit: "+sumFirstAndLastDigit(111));
+        System.out.println("Get even digit sum: " +getEvenDigitSum(123456789));
+        System.out.println("Has shared: "+hasShared(33,13));
+        System.out.println("Has same last digit: "+ hasSameLastDigit(11,15,111));
         printFactors(6);
-        System.out.println(getGreatestCommonDivisor(12, 30));
-        System.out.println(isPerfectNumber(5));
-//        System.out.println(canPack(1,0,4));
+        System.out.println("GCD: "+ getGreatestCommonDivisor(12, 30));
+        System.out.println("Is perfect number "+isPerfectNumber(5));
+        System.out.println(canPack(1,0,4));
+        System.out.println("Largest prime: " + getLargestPrime(21));
+        System.out.println("Get bucket count: "+getBucketCount(3.4, 2.1, 1.5, 2));
+        inputThenPrintSumAndAverage(5);
+        numberToWords(50);
     }
 
     public static void printNumberInWord(int number){
@@ -117,7 +125,34 @@ public class ControlFlowChallenge {
 
     public static boolean hasShared(int num1, int num2){
 
-        return true;
+        int startNum = 10;
+        int endNum = 99;
+        if (!(num1 >= startNum && num1 <= endNum) && !(num2 >= startNum && num2 <= endNum)){
+            return false;
+        }
+
+        int firstNumLastDigit = num1 % 10;
+        int firstNumFirstDigit = num1 / 10;
+
+        int secondNumLastDigit = num2 % 10;
+        int secondNumFirstDigit = num1 / 10;
+
+        return (((firstNumFirstDigit == secondNumFirstDigit) || (firstNumFirstDigit == secondNumLastDigit) )||(firstNumLastDigit == secondNumFirstDigit) || firstNumLastDigit == secondNumLastDigit);
+
+    }
+
+    public static boolean hasSameLastDigit(int num1, int num2, int num3){
+        int startNum = 10;
+        int endNum = 1000;
+        if (!(num1 >= startNum && num1 <= endNum) && !(num2 >= startNum && num2 <= endNum)){
+            return false;
+        }
+
+        int firstNumLastDigit = num1 % 10;
+        int secondNumLastDigit = num2 % 10;
+        int thirdNumLastDigit = num3 % 10;
+
+        return ((firstNumLastDigit == secondNumLastDigit) || (firstNumLastDigit == thirdNumLastDigit) || (secondNumLastDigit == thirdNumLastDigit));
     }
 
     public static void printFactors(int num){
@@ -159,24 +194,150 @@ public class ControlFlowChallenge {
         return num == sum;
     }
 
-//    public static boolean canPack(int bigCount, int smallCount, int goal){
-//        if (bigCount < 0 || smallCount < 0 || goal < 0){
-//            return false;
-//        }
-//        int totalAmt = bigCount * 5 + smallCount;
-//        boolean result = false;
-//        if (totalAmt >= goal) {
-//            int remaining = goal % totalAmt;
-//            if (smallCount >= remaining) {
-//                result = true;
-//            } else {
-//                if (smallCount >= goal - totalAmt) {
-//                    result = true;
-//                }
-//            }
-//
-//        }
-//
-//        return result;
-//    }
+    public static boolean canPack(int bigCount, int smallCount, int goal){
+        if (bigCount < 0 || smallCount < 0 || goal < 0){
+            return false;
+        }
+        boolean result = false;
+        int totalBigWeight = bigCount * 5;
+        if (totalBigWeight >= goal) {
+            int remaining = goal % 5;
+            if (smallCount >= remaining) {
+                result = true;
+            }
+        } else {
+            if (smallCount >= goal - totalBigWeight) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    public static int getLargestPrime( int num){
+        if (num < 2){
+            return -1;
+        }
+        int maxPrime = 1;
+        for (int i = 3; i * i <= num; i += 2) {
+            while (num % i == 0) {
+                maxPrime = i;
+                num /= i;
+            }
+        }
+        if (num > 2) {
+            maxPrime = num;
+        }
+        return maxPrime;
+    }
+
+    public static void inputThenPrintSumAndAverage(int num){
+
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        long avg = 0;
+        int count = 0;
+
+        while (true){
+            boolean isInt = scanner.hasNextInt();
+
+            if (! isInt){
+                break;
+            }
+            sum += scanner.nextInt();
+            count++;
+            scanner.nextLine();
+        }
+
+        if (count > 0) {
+            avg = Math.round((double) sum / count);
+        }
+
+        System.out.println("SUM = " + sum + " AVG = " + avg);
+        scanner.close();
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets){
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0){
+            return -1;
+        }
+        double area = width * height;
+        double remainingArea = area - extraBuckets * areaPerBucket;
+        return getBucketCount(remainingArea, areaPerBucket);
+    }
+    public static int getBucketCount(double width, double height, double areaPerBucket) {
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 ) {
+            return -1;
+        }
+        double area = width * height;
+        return getBucketCount(area, areaPerBucket);
+    }
+    public static int getBucketCount(double area, double areaPerBucket){
+        if (area <= 0 || areaPerBucket < 0){
+            return -1;
+        }
+        return (int)area / (int)areaPerBucket;
+    }
+
+
+    public static int reverse(int num){
+        int reverse = 0;
+        while (num != 0){
+            reverse = reverse * 10 + num % 10 ;
+            num = num / 10;
+        }
+        return reverse;
+    }
+
+    public static int getDigitCount(int num){
+        if (num < 0) {
+            return -1;
+        }
+
+        if (num == 0) {
+            return 1;
+        }
+
+        int count = 0;
+        for (int i = 1; i <= num; i *= 10) {
+            count++;
+        }
+        return count;
+
+    }
+    public static void numberToWords(int num){
+        if (num < 0) {
+            System.out.println("Invalid Value");
+            return;
+        }
+
+        int reverse = reverse(num);
+
+        int leadingZeroes = getDigitCount(num) - getDigitCount(reverse);
+
+        if (num == 0) {
+            System.out.println("Zero");
+            return;
+        }
+
+        while (reverse != 0) {
+            int lastDigit = reverse % 10;
+            switch (lastDigit) {
+                case 0 -> System.out.println("Zero");
+                case 1 -> System.out.println("One");
+                case 2 -> System.out.println("Two");
+                case 3 -> System.out.println("Three");
+                case 4 -> System.out.println("Four");
+                case 5 -> System.out.println("Five");
+                case 6 -> System.out.println("Six");
+                case 7 -> System.out.println("Seven");
+                case 8 -> System.out.println("Eight");
+                case 9 -> System.out.println("Nine");
+            }
+            reverse /= 10;
+        }
+        for (int i = 0; i < leadingZeroes; i++) {
+            System.out.println("Zero");
+        }
+    }
+
 }
