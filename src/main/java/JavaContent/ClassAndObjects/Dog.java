2f23fe4c8092c25b56d19313e5f0f669abc20fca
me-ritikajoshi <me.ritikajoshi@gmail.com>
2025-04-11 11:17:11 +0545

    Static and instance variable

--- src/main/java/JavaContent/ClassAndObjects/Dog.java
+++ src/main/java/JavaContent/ClassAndObjects/Dog.java
@@ -2,11 +2,16 @@ package JavaContent.ClassAndObjects;
 
 public class Dog {
     private static String name;
+    private String breed;
 
-    public Dog(String name){
+    public Dog(String name, String breed){
         Dog.name = name;
+        this.breed = breed;
     }
     public  void printName(){
         System.out.println("Name: "+ name );
+        System.out.println("Breed: "+ breed);
     }
+
+
 }
--- src/main/java/org/example/DogMain.java
+++ src/main/java/org/example/DogMain.java
@@ -4,8 +4,8 @@ import JavaContent.ClassAndObjects.Dog;
 
 public class DogMain {
     public static void main(String[] args) {
-        Dog rex = new Dog("Rex");
-        Dog fluffy = new Dog("Fluffy");
+        Dog rex = new Dog("Rex","qqq");
+        Dog fluffy = new Dog("Fluffy","www");
         rex.printName();
         fluffy.printName();
     }
